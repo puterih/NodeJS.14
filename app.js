e91bda308d173c05ca78fe609a39a5fe473cd898
Puteri Husnul Khotimah <husnulputeri@gmail.com>
2022-07-27 20:11:37 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+// Membuat sebuah routing yg sangat sederhana
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// request menggunakan method get
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Ini Adalah halaman About");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Ini Adalah halaman Contact");
+});
+
+// Menggunakan method use untuk menjalankan/memanggil sebuah middleware/function karna node js nanti semuanya bisa kita anggap sebagai middleware
+app.use("/", (req, res) => {
+  res.status(404);
+  // respon status untuk memberi tahu bahwa halaman ini tidak ada, ceknya di inspect->Network
+  res.send("Test <h1>404</h1>");
+});
+// Test ini akan muncul ketika http://localhost:3000/asd  (asd tidak ada di file) maksudnya Test ini akan selalu dijalankan untuk request apapun
+// disarankan menggunakan use di baris paling bawah
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// // Web Server http://localhost:3000
+// const http = require("http");
+// const fs = require("fs");
+// const port = 3000;
+
+// // Merapikan codingan
+// const renderHTML = (path, res) => {
+//   fs.readFile(path, (err, data) => {
+//     if (err) {
+//       res.writeHead(404);
+//       res.write("Error: file not found");
+//     } else {
+//       res.write(data);
+//     }
+//     res.end();
+//   });
+// };
+
+// // web server yg sederhana
+// http
+//   .createServer((req, res) => {
+//     res.writeHead(200, {
+//       // mengembalikan tipe konten dari text biasa ke text html
+//       "Content-Type": "text/html",
+//     });
+
+//     const url = req.url;
+
+//     switch (url) {
+//       case "/about":
+//         renderHTML("./about.html", res);
+//         break;
+//       case "/contact":
+//         renderHTML("./contact.html", res);
+//         break;
+//       default:
+//         renderHTML("./index.html", res);
+//         break;
+//     }
+
+//     // if (url === "/about") {
+//     //   renderHTML("./about.html", res);
+//     // } else if (url === "/contact") {
+//     //   renderHTML("./contact.html", res);
+//     // } else {
+//     //   // res.write('Hello World!');
+//     //   renderHTML("./index.html", res);
+//     // }
+//   })
+//   .listen(port, () => {
+//     console.log(`Server is listening on port ${port}..`);
+//   });
